f8a86b9715598375d0b6e870a40635a6fd4f1f5a
Vijay <TONY@Mac.lan>
2025-06-07 18:14:07 -0500

    Fixed Dependency issue of password

--- api/extensions/db.py
+++ api/extensions/db.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 import os
 
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://launchhire_user:JCPsai7au1DT7GceyZt84mjusrfjJVBZ@dpg-d11m1k8dl3ps73cud8d0-a.oregon-postgres.render.com/launchhire")
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql+asyncpg://launchhire_user:JCPsai7au1DT7GceyZt84mjusrfjJVBZ@dpg-d11m1k8dl3ps73cud8d0-a.oregon-postgres.render.com/launchhire")
 
 engine = create_async_engine(DATABASE_URL, future=True, echo=False)
 AsyncSessionLocal = sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
