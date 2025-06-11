6da317b9bfece42f435ccd2bb0446b0a658ae49d
Vijay <TONY@Mac.lan>
2025-06-11 13:26:31 -0500

    Added New Render URL

--- api/main.py
+++ api/main.py
@@ -25,9 +25,10 @@ app.add_middleware(
 app.add_middleware(SessionMiddleware, secret_key=os.getenv("SESSION_SECRET", "super-secret-key"))
 
 # --- Static Files (optional) ---
-app.mount("/static", StaticFiles(directory="static"), name="static")
+if os.path.isdir("static"):
+    app.mount("/static", StaticFiles(directory="static"), name="static")
 
-# --- Import Routers (all should have: router = APIRouter() with NO prefix) ---
+# --- Import Routers (all should have: router = APIRouter() with NO prefix in their file) ---
 from api.routers.auth_api import router as auth_router
 from api.routers.resume_api import router as resume_router
 from api.routers.feedback_api import router as feedback_router
@@ -37,7 +38,7 @@ from api.routers.apply_api import router as apply_router
 from api.routers.onboarding_api import router as onboarding_router
 from api.routers.match_api import router as match_router
 
-# --- Master API Router ---
+# --- Master API Router (set ONLY here, no double prefix) ---
 api_router = APIRouter(prefix="/api")
 api_router.include_router(auth_router)
 api_router.include_router(resume_router)
