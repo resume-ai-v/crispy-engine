ff6465874a4e9a30e60ecd43931aa0ac3f7af1a8
Vijay <TONY@Mac.lan>
2025-06-11 12:43:59 -0500

    Added New Render URL

--- ui/src/utils/api.js
+++ ui/src/utils/api.js
@@ -1,4 +1,4 @@
-export const API_BASE = "https://crispy-engine-1.onrender.com/api";
+export const API_BASE = "https://crispy-engine-nx6e.onrender.com/api";
 
 // Helper: get JWT from localStorage
 const getJWT = () => localStorage.getItem("jwt");
